fb2f9b2264cb97bd382d0840ae118923adcf86b2
Vladimir Markevich <vladimir.markevich@gmail.com>
2021-11-08 15:25:43 +0200

    read all distro files contents; more exim logs; added section start/end output

--- discovery.sh
+++ discovery.sh
@@ -2,31 +2,50 @@
 
 REPORT_DIR=results
 FILE=$REPORT_DIR/report.txt 
+
 DEFAULT_EXIM_CONF=/etc/exim.conf
 EXIM_CONF_COPY=$REPORT_DIR/exim.conf.copy
 EMAIL_REGEX="[^@<[ ]+@([^@ ])+\.[^@ >\n]+"
 
+DEFAULT_EXIM_LOG_PATH=/var/log
+DEFAULT_EXIM_LOGS=(exim_mainlog exim_paniclog exim_rejectlog)
+
+sec_start() { 
+  echo -n $@ ".........."
+}
+sec_end() {
+    echo "done"
+}
+
 mkdir -p $REPORT_DIR
 if [[ -f $EXIM_CONF_COPY ]] ; then
-    rm $EXIM_CONF_COPY
+    rm -f $EXIM_CONF_COPY
 fi
 cat /dev/null > $FILE
 
+sec_start "OS Detail"
 echo "--- UNAME ---" >> $FILE 
 uname -a >> $FILE
+sec_end 
 
+
+sec_start "Linux Distro"
 # Linux distro info
 echo "--- LINUX DISTROS ---" >> $FILE
-ls /etc/*release >> $FILE
+for f in `ls /etc/*release` ; do
+   echo "- $f" >> $FILE
+   cat $f >> $FILE
+done
+echo "" >> $FILE
+sec_end
 
 # Exim section
+sec_start "Exim conf"
 echo "--- EXIM ---" >> $FILE
-#echo "DEBUG" 
-#if type -t exim ; then
 if EXIM_BIN=`which exim 2>&1` ; then
     echo $EXIM_BIN >> $FILE
     exim --version 2>&1 >> $FILE
-    echo "---EXIM CONFIG--- " >> $FILE
+    echo "--- EXIM CONFIG --- " >> $FILE
     # Exim config copy 
     if [[ -f $DEFAULT_EXIM_CONF ]] ; then
 	echo $DEFAULT_EXIM_CONF >> $FILE
@@ -42,7 +61,6 @@ else
   echo "Exim not found" >> $FILE
 fi    
 
-
 # Relayhosts
 echo "--- RELAYHOSTS ---" >> $FILE
 cat /etc/relayhosts >> $FILE
@@ -51,13 +69,20 @@ cat /etc/relayhosts >> $FILE
 # SkipSMTPChecks
 echo "--- SKIP SMTP CHECKS HOSTS ---" >> $FILE
 cat /etc/skipsmtpcheckhosts >> $FILE
+sec_end
 
+sec_start "Mail logs"
 # Mail Logs
 echo "--- EXIM MAIL LOGS ---" >> $FILE
-if [[ -f /var/log/exim_mainlog ]]; then
-    echo "/var/log/exim_mainlog -> exim_mainlog-stripped.txt" >> $FILE
-    cat /var/log/exim_mainlog | sed -r "s/$EMAIL_REGEX/$EMAIL_REPLACE_PATTERN/g" > $RESULT_DIR/exim_mainlog-stripped.txt 
-else 
-    echo "/var/log/exim_mainlog NOT FOUND" >> $FILE
-fi
+for f in ${DEFAULT_EXIM_LOGS[@]} ; do
+    sf=$DEFAULT_EXIM_LOG_PATH/$f
+    df=$REPORT_DIR/$f-noemail
+    if [[ -f $sf ]]; then
+        echo "$sf -> $df" >> $FILE
+        cat $sf | sed -r "s/$EMAIL_REGEX/$EMAIL_REPLACE_PATTERN/g" > $df
+    else 
+        echo "$sf NOT FOUND" >> $FILE
+    fi
+done
+sec_end
 
